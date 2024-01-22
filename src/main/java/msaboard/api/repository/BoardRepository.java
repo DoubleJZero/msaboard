e2f768578a26b029e571e1d6cc7de34bce494af7
JandB <blueseajung@naver.com>
2024-01-22 16:06:56 +0900

    config server set

--- src/main/java/msaboard/api/repository/BoardRepository.java
+++ src/main/java/msaboard/api/repository/BoardRepository.java
@@ -3,6 +3,7 @@ package msaboard.api.repository;
 import msaboard.data.entity.TbBoardInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * 게시판 repository
@@ -16,8 +17,11 @@ import org.springframework.data.jpa.repository.Query;
  */
 public interface BoardRepository extends JpaRepository<TbBoardInfo, Long>,BoardRepositoryCustom {
 
-    @Query(nativeQuery = true, value="SELECT NEXTVAL(JANDB.TB_BOARD_INFO_SEQ)")
+    @Query(nativeQuery = true, value="SELECT NEXTVAL ( JANDB.TB_BOARD_INFO_SEQ )")
     Long nextValueBoardId();
+
+    //@Query(nativeQuery = true, value="SELECT NEXTVAL ( :fieldNm )")
+    //Long nextValueBoardId(@Param("fieldNm") String fieldNm);
 
     void deleteByRgstId(String rgstId);
 }
